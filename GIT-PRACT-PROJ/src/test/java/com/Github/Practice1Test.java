f343b3134256197c1fa4e7c732e8e866582681f3
dell <dell@DESKTOP-9D011CU>
2021-11-12 13:40:13 +0530

    2nd commit

---
+++ GIT-PRACT-PROJ/src/test/java/com/Github/Practice1Test.java
@@ -0,0 +1,14 @@
+package com.Github;
+
+import org.testng.annotations.Test;
+
+public class Practice1Test {
+	   @Test
+	   public void smoke()
+	   {
+		   System.out.println("User 2 commit");
+		   
+		   System.out.println("user 2 second commit");
+	   }
+
+}
---
+++ GIT-PRACT-PROJ/src/test/java/com/Github/Practice2Test.java
@@ -0,0 +1,16 @@
+package com.Github;
+
+import org.testng.annotations.Test;
+
+public class Practice2Test {
+	  @Test
+	   public void regression()
+	   {
+		   System.out.println("User 2 commit");
+		   
+		   System.out.println("user 2 second commit");
+		   System.out.println("user 3 second commit");
+	   }
+
+
+}
